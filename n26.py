38f8321c63d60953a6cd59df1174c2d3c41eb2be
damaex17 <maximilian.weber@gmail.com>
2017-05-22 16:04:09 +0200

    adding script

---
+++ n26.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+import requests
+import getpass
+
+
+def login(username,password):
+  h = {"Authorization":"Basic  YW5kcm9pZDpzZWNyZXQ=", "Content-Type":"application/x-www-form-urlencoded"}
+  payload = 'grant_type=password&username=%s&password=%s' % (username,password)
+  r = requests.post('https://api.tech26.de/oauth/token', data=payload, headers=h)
+  return r.json()['access_token']
+
+def get_n26(token,url):
+  h = {'Authorization': 'Bearer %s' % token, 'Content-type': 'application/json'}
+  r = requests.get('%s' % url, headers=h)
+  print r.json()
+
+def get_credentials():
+  username = raw_input("Enter your N26 email address: ")
+  password = getpass.getpass('Enter your password:')
+  return username,password
+
+def main():
+  username,password = get_credentials()
+  token = login(username, password)
+  print get_n26(token,'https://api.tech26.de/api/me')
+  print get_n26(token,'https://api.tech26.de/api/accounts')
+
+
+if __name__ == "__main__":
+    main()
